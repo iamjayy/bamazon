ad74d567ef90a53c3ea1887fdf6743b94094262c
jason <jason_doan94@yahoo.com>
2019-06-06 17:11:23 -0400

    finish

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,73 +13,80 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
    console.log("Connecterd as ID: " + connection.threadId);
 
-   displayProducts()
+   display();
 });
 
 //connection.connect();
 
-//Function for displaying the products
-function displayProducts() {
-    console.log("-----------------------------");
-    console.log("      Welcome To Bamazon    ");
-    console.log("-----------------------------");
-    console.log("");
-    console.log("Find below our Products List");
-    console.log("");
-    //Collect table data from database
-    connection.query("SELECT * FROM products", function (err, response) {
-        if (err) throw err;
-        for (var i = 0; i < response.length; i++) {
-            console.log(response[i].id + ") " + response[i].product_name +" $"+response[i].price + " x " + response[i].stock_quantity);
+var display = function() {
+    connection.query("SELECT * FROM products", function(err, response) {
+      if (err) throw err;
+      console.log("-----------------------------");
+      console.log("      Welcome To Bamazon    ");
+      console.log("-----------------------------");
+      console.log("");
+      console.log("List of products \n");
+      console.log("");
+      var table = new Table({
+        head: ["Product Id", "Product Description", "Cost", "Stock Quantity"],
+        colWidths: [8, 25, 8,],
+        colAligns: ["center", "left", "right"],
+        style: {
+          head: ["aqua"],
+          compact: true
         }
-        console.log("\n");
-        //Once data is collected start prompt to but porducts
-        shopping();
-    });
-}
+      });
+  
+      for (var i = 0; i < response.length; i++) {
+        table.push([response[i].id, response[i].product_name, response[i].price, response[i].stock_quantity]);
+      }
+      console.log(table.toString());
+      console.log("");
+      shopping();
+    }); //End Connection
+  };
 
-function shopping() {
+
+  var shopping = function() {
     connection.query("SELECT * FROM products", function (err, response) {
         if (err) throw err;
 
         inquirer.prompt([{
             name: "id",
             type: "input",
-            message: "What is the ID number of the product you wish to buy?"
+            message: "Please enter the Product Id of the item you wish to purchase!"
         },{
             name: "quantity",
             type: "input",
             message: "How many would you like to purchase?"
         }]).then(function (answer) {
-            var chosenItem = parseInt(answer.id) -1;
+            var selection = parseInt(answer.id) -1;
 
-            var updateQuantity = response[chosenItem].stock_quantity;
+            var updateQuantity = response[selection].stock_quantity;
 
             if(updateQuantity > answer.quantity) {
                 console.log("\n");
                 console.log("Your Order Is In Stock!");
                 updateQuantity-= answer.quantity
 
-                var totalPrice = answer.quantity * response[chosenItem].price;
-                console.log("The total price for " + response[chosenItem].product_name + " is $" +totalPrice)
+                var totalPrice = answer.quantity * response[selection].price;
+                console.log("The total price for " + response[selection].product_name + " is $" +totalPrice)
 
                 var query2 = connection.query(
                     "UPDATE products SET ? WHERE ?",
                     [
-                        {
-                            stock_quantity: updateQuantity
-                        },
-                        {
-                            product_name:response[chosenItem].product_name
-                        }
+                        { stock_quantity: updateQuantity },
+
+                        {product_name:response[selection].product_name}
                     ],
                     function(err, res) {
                         console.log(" YOUR ORDER HAS BEEN PLACED! \n");
                     }
                 )
             } else {
-                console.log("WE ARE OUT OF STOCK " + response[chosenItem].product_name + " Insufficient quantity! ")
+                console.log("WE ARE OUT OF STOCK " + response[selection].product_name + " Insufficient quantity! ")
             }
+            connection.end();
         });
     });
 }
